f7400a5ab718600565a65342a8c27d1fbad9eca9
1459970707@qq.com <1459970707@qq.com>
2022-04-21 15:28:28 +0800

    first commit

---
+++ js/defaultData.js
@@ -0,0 +1,22 @@
+export {
+  defaultMusicSrc
+}
+//默认的音乐文件地址
+var defaultMusicSrc = [
+  { "src": "../music/default/0.mp3" },
+  { "src": "../music/default/1.mp3" },
+  { "src": "../music/default/2.mp3" },
+  { "src": "../music/default/3.mp3" },
+  { "src": "../music/default/4.mp3" },
+  { "src": "../music/default/5.mp3" },
+  { "src": "../music/default/6.mp3" },
+  { "src": "../music/default/7.mp3" },
+  { "src": "../music/default/8.mp3" },
+  { "src": "../music/default/9.mp3" },
+  { "src": "../music/default/10.mp3" },
+  { "src": "../music/default/11.mp3" },
+  { "src": "../music/default/12.mp3" },
+  { "src": "../music/default/13.mp3" },
+  { "src": "../music/default/14.mp3" },
+]
+
---
+++ js/index.js
@@ -0,0 +1,43 @@
+import {
+  defaultMusicSrc,
+} from './defaultData.js';
+
+var $keysList = document.getElementsByClassName('key')
+
+function MusicPlay(defaultMusicSrc, key) {
+  return new Promise((resplve, reject) => {
+    if (defaultMusicSrc[key]) {
+      let newAudio = new Audio();
+      newAudio.src = defaultMusicSrc[key].src; //设置播放的地址
+      newAudio.autoplay = true; //加载完成后立即播放
+      // newAudio.play(); //播放音频
+      newAudio.addEventListener("ended", () => { //音频播放完毕
+        newAudio.removeEventListener("ended", () => { })
+        newAudio = null; //销毁
+      })
+      resplve();
+    } else {
+      reject("无此音乐文件");
+    }
+  });
+}
+
+//键盘控件点击
+function clickMusicKnock(e) {
+  let key = e.target.className.split("num")[1]
+  //调用播放的方法
+  MusicPlay(defaultMusicSrc, key).then(
+    () => {
+      // console.log("播放成功")
+    },
+    (err) => {
+      // console.log("播放失败:", err)
+    },
+  )
+}
+
+for (let i in $keysList) {
+  if (typeof $keysList[i] == "object") {
+    $keysList[i].addEventListener("touchstart", (e) => clickMusicKnock(e))
+  }
+}
