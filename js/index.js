8e28ba7f977e820c4ae5a8853f335a3824f185ea
1459970707@qq.com <1459970707@qq.com>
2022-04-22 09:29:56 +0800

    master

--- js/index.js
+++ js/index.js
@@ -2,29 +2,33 @@ import {
   defaultMusicSrc,
 } from './defaultData.js';
 
-var $keysList = document.getElementsByClassName('key')
+var $keysList = document.getElementsByClassName('key');
 
 function MusicPlay(defaultMusicSrc, key) {
   return new Promise((resplve, reject) => {
     if (defaultMusicSrc[key]) {
-      let newAudio = new Audio();
-      newAudio.src = defaultMusicSrc[key].src; //设置播放的地址
-      newAudio.autoplay = true; //加载完成后立即播放
-      // newAudio.play(); //播放音频
+      // console.log(defaultMusicSrc[key].data);
+      //创建媒体对象
+      let newAudio = new Audio(defaultMusicSrc[key].data);
+      //调用play方法
+      newAudio.autoplay = true;
+      // let newAudio = new Audio();//旧方法
+      // newAudio.src = defaultMusicSrc[key].src; //设置播放的地址
+      // newAudio.autoplay = true; //加载完成后立即播放// newAudio.play(); //播放音频
       newAudio.addEventListener("ended", () => { //音频播放完毕
-        newAudio.removeEventListener("ended", () => { })
+        newAudio.removeEventListener("ended", () => { });
         newAudio = null; //销毁
-      })
+      });
       resplve();
     } else {
       reject("无此音乐文件");
-    }
+    };
   });
-}
+};
 
 //键盘控件点击
 function clickMusicKnock(e) {
-  let key = e.target.className.split("num")[1]
+  let key = e.target.className.split("num")[1];
   //调用播放的方法
   MusicPlay(defaultMusicSrc, key).then(
     () => {
@@ -34,7 +38,7 @@ function clickMusicKnock(e) {
       // console.log("播放失败:", err)
     },
   )
-}
+};
 
 for (let i in $keysList) {
   if (typeof $keysList[i] == "object") {
